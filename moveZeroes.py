5a011117287addb0eef21a5687cb76741c0191ba
Charishma Shivani <107106804+charishmashivani@users.noreply.github.com>
2024-07-18 21:28:40 +0530

    Create moveZeroes.py

---
+++ moveZeroes.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        n = 0
+
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[n] = nums[n], nums[i]
+                n+=1
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        
