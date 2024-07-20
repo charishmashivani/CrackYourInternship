94b62f6b393c1abcd77e8a819a666102d462a3d5
Charishma Shivani <107106804+charishmashivani@users.noreply.github.com>
2024-07-20 22:33:07 +0530

    Create isValid.py

---
+++ isValid.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        bracketMap = {')': '(', '}': '{', ']': '['}
+        stack = []
+        for char in s:
+            if char in bracketMap:
+                topElement = stack.pop() if stack else '#'
+                if bracketMap[char] != topElement:
+                    return False
+            else:
+                stack.append(char)
+        return not stack
