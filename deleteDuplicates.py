efa2433b3300d0f34cbad40ac9506c4b08d413fc
Charishma Shivani <107106804+charishmashivani@users.noreply.github.com>
2024-07-21 14:03:53 +0530

    Create deleteDuplicates.py

---
+++ deleteDuplicates.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def deleteDuplicates(self, head):
+        current = head
+        
+        while current and current.next:
+            if current.val == current.next.val:
+                # Skip the next node since it's a duplicate
+                current.next = current.next.next
+            else:
+                # Move to the next node
+                current = current.next
+        
+        return head
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        
