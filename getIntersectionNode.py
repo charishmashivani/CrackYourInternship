effd15005912defcded3e3a347d19d990e045772
Charishma Shivani <107106804+charishmashivani@users.noreply.github.com>
2024-07-27 20:58:47 +0530

    https://leetcode.com/problems/intersection-of-two-linked-lists/submissions/1335223841/

---
+++ getIntersectionNode.py
@@ -0,0 +1,24 @@
+#Day 10
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type headA: ListNode
+        :type headB: ListNode
+        :rtype: ListNode
+        """
+        if not headA or not headB:
+            return None
+
+        pointerA, pointerB = headA, headB
+
+        while pointerA != pointerB:
+            pointerA = pointerA.next if pointerA else headB
+            pointerB = pointerB.next if pointerB else headA
+
+        return pointerA
