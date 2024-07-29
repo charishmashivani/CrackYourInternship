2c21e3d1ee0d72d8bed69880f372d5d921c5a288
Charishma Shivani <107106804+charishmashivani@users.noreply.github.com>
2024-07-29 21:07:17 +0530

    https://leetcode.com/problems/add-two-numbers/submissions/1337425722/

---
+++ addTwoNumbers.py
@@ -0,0 +1,30 @@
+#Day 12
+
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummy_head = ListNode(0)
+        current = dummy_head
+        carry = 0
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            total = val1 + val2 + carry
+            carry = total // 10
+            sum_val = total % 10
+            current.next = ListNode(sum_val)
+            current = current.next
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+        return dummy_head.next
