f40a4ec3b54817e032aa6c3c486420c68d52287d
Charishma Shivani <107106804+charishmashivani@users.noreply.github.com>
2024-08-02 17:14:01 +0530

    https://leetcode.com/problems/range-sum-of-bst/submissions/1341787294/

--- rangeSumBST.py
+++ rangeSumBST.py
@@ -15,26 +15,20 @@ class TreeNode:
 class Solution(object):
     def rangeSumBST(self, root, low, high):
         def dfs(node):
-            # Base case: if the node is None, return 0
             if not node:
                 return 0
             
             total_sum = 0
             if low <= node.val <= high:
-                # If node's value is within the range, add it to the sum
                 total_sum += node.val
             
-            # If node's value is greater than low, traverse left subtree
             if node.val > low:
                 total_sum += dfs(node.left)
             
-            # If node's value is less than high, traverse right subtree
             if node.val < high:
                 total_sum += dfs(node.right)
             
             return total_sum
-        
-        # Start the DFS traversal from the root
         return dfs(root)
 
         """
