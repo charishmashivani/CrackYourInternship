be25387cf6dca3d7cd34ab9f6415f080f1dbd00e
Charishma Shivani <107106804+charishmashivani@users.noreply.github.com>
2024-08-02 16:29:58 +0530

    Create invertTree.py

---
+++ invertTree.py
@@ -0,0 +1,22 @@
+#Day 16
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def invertTree(self, root):
+        # Base case: if the root is None, just return None
+        if root is None:
+            return None
+        
+        # Swap the left and right children
+        root.left, root.right = root.right, root.left
+        
+        # Recursively invert the left and right subtrees
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        
+        return root
