fb33dd70fa45bc551afe36635a3fddea76d7f890
Charishma Shivani <107106804+charishmashivani@users.noreply.github.com>
2024-08-04 22:42:34 +0530

    https://leetcode.com/problems/lru-cache/submissions/1344498219/

---
+++ LRUCache.py
@@ -0,0 +1,62 @@
+#Day 18
+
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+class LRUCache(object):
+
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.capacity = capacity
+        self.cache = {}
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _remove(self, node):
+        prev, next = node.prev, node.next
+        prev.next, next.prev = next, prev
+
+    def _add(self, node):
+        node.prev, node.next = self.head, self.head.next
+        self.head.next.prev, self.head.next = node, node
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        if key in self.cache:
+            node = self.cache[key]
+            self._remove(node)
+            self._add(node)
+            return node.value
+        return -1
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: None
+        """
+        if key in self.cache:
+            node = self.cache[key]
+            self._remove(node)
+            node.value = value
+            self._add(node)
+        else:
+            if len(self.cache) >= self.capacity:
+                lru = self.tail.prev
+                self._remove(lru)
+                del self.cache[lru.key]
+            
+            node = Node(key, value)
+            self._add(node)
+            self.cache[key] = node
