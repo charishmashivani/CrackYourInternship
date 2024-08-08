56e7ba1fc80227da476662ff43804d32f5669503
Charishma Shivani <107106804+charishmashivani@users.noreply.github.com>
2024-08-08 21:27:43 +0530

    https://leetcode.com/problems/ones-and-zeroes/submissions/1349040027/

---
+++ findMaxForm.py
@@ -0,0 +1,21 @@
+#Day 22
+
+class Solution(object):
+    def findMaxForm(self, strs, m, n):
+        """
+        :type strs: List[str]
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        
+        for s in strs:
+            zeros = s.count('0')
+            ones = s.count('1')
+            
+            for i in range(m, zeros - 1, -1):
+                for j in range(n, ones - 1, -1):
+                    dp[i][j] = max(dp[i][j], dp[i - zeros][j - ones] + 1)
+        
+        return dp[m][n]
